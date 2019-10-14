c13577df82cf8a60bb46c7cd5267d302fda31d40
dDua <dheeru_dua@hotmail.com>
2019-10-14 15:54:10 -0700

    push path issues

--- run_model.sh
+++ run_model.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://nabert-model.s3.us-east-2.amazonaws.com/model.tar.gz
-cp /orb-devset/all_dev.json /tmp/input.json
-# to test without running the model
-#python predictor.py model.tar.gz /tmp/input.json predictions.json
+wget https://sparc-dataset.s3.us-east-2.amazonaws.com/model.tar.gz
+# Copying the input file to /tmp because /orb-dev in read-only.
+cp /orb-dev/dev_noanswer.json /tmp/input.json
+python predictor.py model.tar.gz /tmp/input.json predictions.json
 cp predictions.json /results/predictions.json
