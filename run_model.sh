b01e9c7c00f0968c171ae1609e4706ae6e86f15c
AGottu <45110948+AGottu@users.noreply.github.com>
2019-11-18 13:57:52 -0800

    Update run_model.sh

--- run_model.sh
+++ run_model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://sparc-dataset.s3.us-east-2.amazonaws.com/model.tar.gz
+wget https://ropes-model.s3-us-west-1.amazonaws.com/model.tar.gz
 # Copying the input file to /tmp because /orb-dev in read-only.
 cp /orb-data/orb_nolabels.jsonl /tmp/input.jsonl
 python predictor.py model.tar.gz /tmp/input.jsonl predictions.json
